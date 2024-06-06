94ee249b2fe9ba656e4a86cfada4e25b13aa0a80
oxalonzo <orlandoalonzo40@gmail.com>
2024-06-06 10:50:00 -0400

    subiendo archivo

---
+++ hola.php
@@ -0,0 +1,14 @@
+<?php 
+include 'include/header.php'
+?>
+
+
+
+    <h1 class="hola">holas</h1>
+
+
+<?php 
+include 'include/footer.php'
+?>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hola = document.querySelector('.hola');
+const body = document.getElementsByName('body');
+
+console.log(body);
+
+
+
+hola.addEventListener('click', () => {
+hola.style.background = '#000';
+})
